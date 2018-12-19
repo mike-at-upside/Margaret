490394b7e0d3b2bd1a90e673991735329c0dd5b2
Mike Lowe <mikel@upsideenergy.co.uk>
2018-12-19 14:57:08 +0000

    Add basic slack response

---
+++ src/main/java/com/blackeagles/margaret/model/SlackResponse.java
@@ -0,0 +1,34 @@
+package com.blackeagles.margaret.model;
+
+import java.util.Objects;
+
+public class SlackResponse {
+
+  private String text;
+
+  public String getText() {
+    return text;
+  }
+
+  public SlackResponse setText(final String text) {
+    this.text = text;
+    return this;
+  }
+
+  @Override
+  public final boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof SlackResponse)) {
+      return false;
+    }
+    final SlackResponse that = (SlackResponse) o;
+    return Objects.equals(text, that.text);
+  }
+
+  @Override
+  public final int hashCode() {
+    return Objects.hash(text);
+  }
+}
--- src/main/java/com/blackeagles/margaret/rest/TestController.java
+++ src/main/java/com/blackeagles/margaret/rest/TestController.java
@@ -3,12 +3,15 @@ package com.blackeagles.margaret.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.blackeagles.margaret.model.SlackResponse;
+
 @RestController
 public class TestController {
 
   @GetMapping("/worst")
-  public String worst() {
-    return "Daniel is worst";
+  public SlackResponse worst() {
+    return new SlackResponse()
+        .setText("Daniel is worst");
   }
 
 }
