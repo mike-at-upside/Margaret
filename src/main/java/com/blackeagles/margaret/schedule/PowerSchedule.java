92d2abe6a89d37ab8b42ceaf84854d3e9e24ac78
Matthew Potts <matt.potts@upsideenergy.co.uk>
2018-12-19 16:44:35 +0000

    PowerSchedule actually running

--- src/main/java/com/blackeagles/margaret/schedule/PowerSchedule.java
+++ src/main/java/com/blackeagles/margaret/schedule/PowerSchedule.java
@@ -1,10 +1,12 @@
 package com.blackeagles.margaret.schedule;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PowerSchedule {
 
-  @Scheduled(fixedDelay = 5)
+  @Scheduled(fixedDelay = 5000)
   public void sendStatsToSlack() {
     System.out.println("Fixed delay task - " + System.currentTimeMillis() / 1000);
   }
