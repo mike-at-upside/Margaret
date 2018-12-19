41cc2a27ce1a54c1fc246e567a49c21e4a3df1c5
Mike Lowe <mikel@upsideenergy.co.uk>
2018-12-19 13:49:47 +0000

    Try again

---
+++ src/main/java/com/blackeagles/margaret/MargaretApplication.java
@@ -0,0 +1,14 @@
+package com.blackeagles.margaret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MargaretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MargaretApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/blackeagles/margaret/MargaretApplicationTests.java
@@ -0,0 +1,17 @@
+package com.blackeagles.margaret;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MargaretApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
