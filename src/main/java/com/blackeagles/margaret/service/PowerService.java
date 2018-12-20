e47315eeb236094578cad6140527ad73cf637946
Mike Lowe <mikel@upsideenergy.co.uk>
2018-12-20 09:59:56 +0000

    Adding worst minutes

--- src/main/java/com/blackeagles/margaret/service/PowerService.java
+++ src/main/java/com/blackeagles/margaret/service/PowerService.java
@@ -28,21 +28,24 @@ public class PowerService {
       ZonedDateTime begin = end.minusMinutes(minutes);
       final List<FloorPower> meanPowers = influxService.getMeanPower(begin, end);
       final FloorPower bestFloor = meanPowers.get(0);
-      return String.format(":party-parrot: Floor %s performed best using %s kW over the last %d minutes :party-parrot:",
+      return String.format(
+          ":party-parrot: Floor %s performed best using an average of %s kW over the last %d minutes :party-parrot:",
           bestFloor.getId(), bestFloor.getPowerKilowatts(), minutes);
     } else if (normalisedText.equals("last hour") || normalisedText.equals("one hour")) {
       ZonedDateTime end = ZonedDateTime.now();
       ZonedDateTime begin = end.minusHours(1);
       final List<FloorPower> meanPowers = influxService.getMeanPower(begin, end);
       final FloorPower bestFloor = meanPowers.get(0);
-      return String.format(":party-parrot: Floor %s performed best using %s kW over the last hour :party-parrot:",
-          bestFloor.getId(), bestFloor.getPowerKilowatts());
+      return String
+          .format(":party-parrot: Floor %s performed best using an average of %s kW over the last hour :party-parrot:",
+              bestFloor.getId(), bestFloor.getPowerKilowatts());
     } else if (normalisedText.equals("day") || normalisedText.equals("last day") || normalisedText.equals("24 hours")) {
       ZonedDateTime end = ZonedDateTime.now();
       ZonedDateTime begin = end.minusHours(24);
       final List<FloorPower> meanPowers = influxService.getMeanPower(begin, end);
       final FloorPower bestFloor = meanPowers.get(0);
-      return String.format(":party-parrot: Floor %s performed best using %s kW over the last 24 hours :party-parrot:",
+      return String.format(
+          ":party-parrot: Floor %s performed best using an average of %s kW over the last 24 hours :party-parrot:",
           bestFloor.getId(), bestFloor.getPowerKilowatts());
     } else {
       return "couldn't get best for period: " + normalisedText;
@@ -52,19 +55,28 @@ public class PowerService {
   public String worstPeriod(String text) {
     String normalisedText = normaliseText(text);
 
-    if (normalisedText.equals("last hour") || normalisedText.equals("one hour")) {
+    if (PATTERN.matcher(normalisedText).matches()) {
+      int minutes = Integer.parseInt(normalisedText.replaceAll("\\D+", ""));
+      ZonedDateTime end = ZonedDateTime.now();
+      ZonedDateTime begin = end.minusMinutes(minutes);
+      final List<FloorPower> meanPowers = influxService.getMeanPower(begin, end);
+      final FloorPower worstFloor = meanPowers.get(meanPowers.size() - 1);
+      return String.format(
+          ":skull: Floor %s performed worst using an average of %s kW over the last %d minutes :skull:",
+          worstFloor.getId(), worstFloor.getPowerKilowatts(), minutes);
+    } else if (normalisedText.equals("last hour") || normalisedText.equals("one hour")) {
       ZonedDateTime end = ZonedDateTime.now();
       ZonedDateTime begin = end.minusHours(1);
       final List<FloorPower> meanPowers = influxService.getMeanPower(begin, end);
       final FloorPower worstFloor = meanPowers.get(meanPowers.size() - 1);
-      return String.format(":skull: Floor %s performed worst using %s kW over the last hour :skull:",
+      return String.format(":skull: Floor %s performed worst using an average of %s kW over the last hour :skull:",
           worstFloor.getId(), worstFloor.getPowerKilowatts());
     } else if (normalisedText.equals("day") || normalisedText.equals("last day") || normalisedText.equals("24 hours")) {
       ZonedDateTime end = ZonedDateTime.now();
       ZonedDateTime begin = end.minusHours(24);
       final List<FloorPower> meanPowers = influxService.getMeanPower(begin, end);
       final FloorPower worstFloor = meanPowers.get(meanPowers.size() - 1);
-      return String.format(":skull: Floor %s performed worst using %s kW over the last 24 hours :skull:",
+      return String.format(":skull: Floor %s performed worst using an average of %s kW over the last 24 hours :skull:",
           worstFloor.getId(), worstFloor.getPowerKilowatts());
     } else {
       return "couldn't get worst for period: " + normalisedText;
